3b417dabf40d19f36e45e5a1e78bf8b29718e3e1
infinitesky4444 <amureke71@gmail.com>
2017-03-24 23:12:49 +0100

    moving htmlbuild js

--- src/app/components/page/page.component.ts
+++ src/app/components/page/page.component.ts
@@ -19,6 +19,7 @@ import '../../js/paperstack/main.js';
 
 declare var carousel: any;
 declare var $:any;
+declare var buildcontenthtml:any;
 
 @Pipe ({ name: 'safeHtml'})
 
@@ -104,8 +105,9 @@ export class PageComponent implements AfterViewChecked {
 
             //TEST ON JAVASCRIPT OR HTML STRING
             //let contentGrid = umbpagedata.data.bodyContentGrid;
-            let contentGrid = builder(umbpagedata.data.bodyContentGridJson);
 
+            //buildcontenthtml function is added on index.html in js file named htmlbuilder.js
+            let contentGrid = buildcontenthtml(umbpagedata.data.bodyContentGridJson);
           //  this.contentGrid = contentGrid.replace('{{renderformid_1}}', '<div id="formContainer" #formContainer></div>');
             this.contentGrid = contentGrid;
             this.seoService.setMetaElement("metaDescription", umbpagedata.data.metaDescription);
@@ -125,165 +127,6 @@ export class PageComponent implements AfterViewChecked {
         this.seoService.setTitle(data.title);
       }
     });
-
-
-    function builder(j){
-      var s = "<div>";
-
-      if (j) {
-          //if one col
-          if (Object.keys(j.sections).length <= 1) {
-            for (let i = 0; i < Object.keys(j.sections).length; i++){
-              var array = j.sections[i].rows;
-              for (let i = 0; i < array.length; i++) {
-                s += renderRow(array[i], true);
-              }
-            }
-          }
-          else {
-            s += "<p>We need to take care of 2-12 col. stuff</p>";
-          }
-      }
-      else {
-          s += "<p>EMPTY</p>";
-      }
-
-      s += "</div>";
-
-      return s;
-    }
-
-    function renderRow(row, singleColumn) {
-
-      var rowhtml = "";
-
-      rowhtml += "<div " + RenderElementAttributes(row) + ">";
-      if (singleColumn) {
-        rowhtml += "<div class='container'>";
-      }
-      rowhtml += "<div class='row'>";
-        for (let i = 0; i < row.areas.length; i++) {
-          rowhtml += "<div class='col s" + row.areas[i].grid + "'>";
-          rowhtml += "<div " + RenderElementAttributes(row.areas[i]) + ">";
-
-          for (let e = 0; e < row.areas[i].controls.length; e++) {
-              var control = row.areas[i].controls[e];
-              if (control != null && control.editor != null && control.editor.view != null) {
-                //console.log(control);
-                //console.log(control.editor);
-                //console.log(control.editor.view);
-                rowhtml += editorview(control);
-              }
-          }
-
-          rowhtml += "</div>";
-          rowhtml += "</div>";
-        }
-      rowhtml += "</div>";
-      if (singleColumn) {
-        rowhtml += "</div>";
-      }
-      rowhtml += "</div>";
-
-      return rowhtml;
-    }
-
-
-    function RenderElementAttributes(contentItem){
-      var r = "";
-
-      var cfg = contentItem.config;
-      if (typeof cfg !== "undefined"){
-        Object.keys(cfg).forEach(function (key) {
-            r += key + "='" + cfg[key].toLowerCase().replace(" ", "-") + "'";
-        });
-      }
-
-      var style = contentItem.styles;
-      if (typeof style !== "undefined"){
-        r += "style='";
-        Object.keys(style).forEach(function (key) {
-          if (key == "background-image") {
-              style[key] = "url(http://umb.dynamikfabrikken.com" + style[key].replace("url(", "");
-          }
-          r +=  key + ": " + style[key] + "; ";
-        });
-        r += "'";
-      }
-
-      return r;
-    }
-
-    function editorview(contentItem){
-      var e = ""
-      var type = contentItem.editor.alias;
-
-      try {
-            if (type == "rte") {
-            // line 139 - TemplateUtilities.ParseInternalLinks not added
-            e += contentItem.value.replace("src=\"/", "src=\"" + "http://umb.dynamikfabrikken.com" + "/");
-            }
-            else if (type == "macro"){
-              var macroalias = contentItem.value.macroAlias;
-              if (macroalias == "slider") {
-                e += buildslider();
-              }
-            }
-            else{
-              console.log(type);
-            }
-      }
-      catch(err) {
-
-      }
-
-      return e;
-    }
-
-    function buildslider(){
-      var slider = "";
-
-
-            slider += "<div materialize='carousel' class='carousel'>";
-
-            slider += "<div class='carousel-fixed-item center'>";
-            slider += "<a class='btn waves-effect white grey-text darken-text-2'>button</a>";
-            slider += "</div>";
-
-            for (let i = 0; i < 3; i++) {
-              slider += "<div class='carousel-item red white-text' href='#" + i + "'>";
-              slider += "<h2>First Panel</h2><p class='white-text'>This is your first panel</p>";
-              slider += "</div>";
-            }
-
-            slider += "</div>";
-
-
-            slider += "<div appCarouselSpy id='full-carousel' class='carousel carousel-slider center' data-indicators='true'>";
-              slider += "<div class='carousel-fixed-item center'>";
-                  slider += "<a class='btn waves-effect white grey-text darken-text-2'>button</a>";
-              slider += "</div>";
-              slider += "<div class='carousel-item red white-text' href='#one!'>";
-                  slider += "<h2>First Panel</h2>";
-                  slider += "<p class='white-text'>This is your first panel</p>";
-              slider += "</div> ";
-              slider += "<div class='carousel-item amber white-text' href='#two!'>";
-                  slider += "<h2>Second Panel</h2>";
-                  slider += "<p class='white-text'>This is your second panel</p>";
-              slider += "</div>";
-              slider += "<div class='carousel-item green white-text' href='#three!'>";
-                  slider += "<h2>Third Panel</h2>";
-                  slider += "<p class='white-text'>This is your third panel</p>";
-              slider += "</div>";
-              slider += "<div class='carousel-item blue white-text' href='#four!'>";
-                  slider += "<h2>Fourth Panel</h2>";
-                  slider += "<p class='white-text'>This is your fourth panel</p>";
-              slider += "</div>";
-              slider += "</div>";
-
-      return slider;
-    }
-
   }
 
   ngAfterViewChecked() {
@@ -293,10 +136,4 @@ export class PageComponent implements AfterViewChecked {
       this.lastFormContent = tempFormContent;
     }*/
   }
-
-
-  //html render stuff
-
-
-
 }
