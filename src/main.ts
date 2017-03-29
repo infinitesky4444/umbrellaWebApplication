47202a544a03a83614a5ce7ec6be75a4cc9c81a4
infinitesky4444 <amureke71@gmail.com>
2017-03-29 23:36:22 +0800

    fix css load(temporary)

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,16 @@ import { environment } from './environments/environment';
 import { AppModule } from './app/';
 import loadScript from 'load-script';
 import loadCss from 'load-css-file';
-
 import Settings from './app/services/settings';
 
 const { scripts, styles } = Settings[window.location.hostname];
-for (let key in scripts)
-  loadScript(scripts[key]);
 for (let key in styles)
   loadCss(styles[key]);
+for (let key in scripts)
+  loadScript(scripts[key]);
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+setTimeout(() => platformBrowserDynamic().bootstrapModule(AppModule), 500);
