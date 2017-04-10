13613d9b000eebe8472eab49d03772c4911c2e9b
infinitesky4444 <amureke71@gmail.com>
2017-04-10 15:33:51 +0200

    font handeling + colors and more

--- src/app/js/material/index.js
+++ src/app/js/material/index.js
@@ -1,4 +1,4 @@
-window.getStyle = function (indexNum, isLast) {
+window.getStyle = function(indexNum, isLast) {
   var sInitial = [
     [100, 100, 50, 100],
     [50, 100, 50, 50]
@@ -11,13 +11,35 @@ window.getStyle = function (indexNum, isLast) {
     sInitial[initPoint][0] / zoomLevel - sInitial[initPoint][2] / zoomLevel,
     sInitial[initPoint][1] / zoomLevel - sInitial[initPoint][3] / zoomLevel
   ];
-  var colors = ['#ff8888', '#ffff88', '#ffffff', '#88ff88', '#88ffff', '#8888ff'];
+  var bgcolors = ['#F5F5F5', '#7e7e7e', '#EEEEEE', '#212121', '#616161', '#7e7e7e'];
+  var colors = ['#000', '#000', '#000', '#fff', '#fff', '#000'];
   return {
     left: sPos[0] + "%",
     top: sPos[1] + "%",
     right: (isLast ? "0" : sPos[2]) + "%",
     bottom: (isLast ? "0" : sPos[3]) + "%",
-    backgroundColor: colors[indexNum % colors.length]
+    backgroundColor: bgcolors[indexNum % bgcolors.length],
+    color: colors[indexNum % colors.length]
   };
+}
 
+// font handeling
+$(function() {
+  $(function() {
+    initfontsize();
+  });
+  window.addEventListener("resize", function() {
+    initfontsize();
+  }, false);
+});
+
+
+
+function initfontsize() {
+  var bwidth = $("body").width();
+  var size = size = 100;
+  if (bwidth < 768) {
+    size = 50;
+  }
+  $('html').css('font-size', size + "%");
 }
